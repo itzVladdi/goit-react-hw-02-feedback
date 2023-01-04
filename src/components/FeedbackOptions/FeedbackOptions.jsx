39a3c8e51fd5f1875108a5b792659245648f9f4f
Vladislav Marinin <czargion@gmail.com>
2023-01-04 10:48:23 +0200

    adds styles

--- src/components/FeedbackOptions/FeedbackOptions.jsx
+++ src/components/FeedbackOptions/FeedbackOptions.jsx
@@ -1,11 +1,25 @@
+import PropTypes from 'prop-types';
+
+import css from './FeedbackOptions.module.css';
+
 export function FeedbackOptions({ options, onLeaveFeedback }) {
   return (
-    <>
+    <div className={css.wrapper}>
       {options.map(option => (
-        <button key={option} name={option} onClick={onLeaveFeedback}>
+        <button
+          className={css.btn}
+          key={option}
+          name={option}
+          onClick={onLeaveFeedback}
+        >
           {option}
         </button>
       ))}
-    </>
+    </div>
   );
 }
+
+FeedbackOptions.propTypes = {
+  options: PropTypes.arrayOf(PropTypes.string).isRequired,
+  onLeaveFeedback: PropTypes.func.isRequired,
+};
--- src/components/Section/Section.jsx
+++ src/components/Section/Section.jsx
@@ -1,8 +1,15 @@
+import PropTypes from 'prop-types';
+import css from './Section.module.css';
+
 export function Section({ title, children }) {
   return (
-    <section>
-      <h2>{title}</h2>
+    <section className={css.section}>
+      <h2 className={css.title}>{title}</h2>
       {children}
     </section>
   );
 }
+Section.propTypes = {
+  title: PropTypes.string.isRequired,
+  children: PropTypes.node.isRequired,
+};
